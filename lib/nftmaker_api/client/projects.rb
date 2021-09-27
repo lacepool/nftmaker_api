2c61584510dac661539fb20b256de9fe5294e8db
Robin Böning <robin.boening@gmail.com>
2021-09-27 15:48:50 +0200

    add CreateProject endpoint

--- lib/nftmaker_api/client/projects.rb
+++ lib/nftmaker_api/client/projects.rb
@@ -4,6 +4,13 @@ module NftmakerApi
       @client = client
     end
 
+    def create(name:, policy_expires: true, policy_locks_at: Time.now.advance(months: 6), address_expires_in: 10)
+      body = {projectname: name, maxNftSupply: 1, policyExpires: policy_expires, addressExpiretime: address_expires_in}
+      body.merge!({ policyLocksDateTime: policy_locks_at.iso8601 }) if policy_expires
+
+      @client.post "/CreateProject/#{@client.api_key}", body.to_json
+    end
+
     def list
       @client.get "ListProjects/#{@client.api_key}"
     end
