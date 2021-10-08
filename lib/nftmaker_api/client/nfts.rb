35933a10688b80ed6502ba5fab2a811561295e9f
Robin Böning <robin.boening@gmail.com>
2021-10-08 23:37:08 +0200

    support nft creation endpoint

--- lib/nftmaker_api/client/nfts.rb
+++ lib/nftmaker_api/client/nfts.rb
@@ -5,6 +5,25 @@ module NftmakerApi
       @project_id = project_id
     end
 
+    def create(name:, file_url:, mime_type:, description: nil, metadata_vars: nil)
+      file_url_hash = if file_url.start_with? "ipfs"
+        { fileFromIPFS: file_url }
+      else
+        { fileFromsUrl: file_url }
+      end
+
+      body = {
+        assetName: name,
+        previewImageNft: {
+          mimetype: mime_type,
+          description: description,
+          metadataPlaceholder: metadata_vars,
+        }.merge!(file_url_hash)
+      }
+
+      @client.post "UploadNft/#{@client.api_key}/#{@project_id}", body
+    end
+
     def list(state: :all)
       @client.get "GetNfts/#{@client.api_key}/#{@project_id}/#{state}"
     end
